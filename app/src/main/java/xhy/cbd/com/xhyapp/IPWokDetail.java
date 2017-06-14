f12dfb4d30dd0cb42698b3201baa4d444b41b493
lisheng <1021337635@qq.com>
2017-06-14 17:11:13 +0800

    Version1.0

--- app/src/main/java/xhy/cbd/com/xhyapp/IPWokDetail.java
+++ app/src/main/java/xhy/cbd/com/xhyapp/IPWokDetail.java
@@ -24,6 +24,8 @@ public class IPWokDetail extends AppCompatActivity {
         texview_workName=(TextView)findViewById(R.id.texview_workName);
         texview_workName.setText(workName);
         btn_goback=(ImageButton)findViewById(R.id.btn_goback);
+
+
         btn_goback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
